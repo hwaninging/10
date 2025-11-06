7445c36d500cb9d77737df8a1b047ac04a542af0
hwaninging <hwanhee@sookmyung.ac.kr>
2025-11-06 15:44:54 +0900

    실습 5:  입력 받은 단어를 파일로 출력
    입력한거 그대로 출력됨.
    sample.txt

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//4. 문자열 길이 계산 
-//실질적인 문자열 길이를 알수 있다. 
+//5. 입력 받은 단어를 파일로 출력 
 
 int main(int argc, char *argv[])
 {
-    char str[30] = "happy C programming";
+    char input[100];
+    FILE* fp;
+    int i;
     
-    printf("string %s length: %i\n", str, strlen(str));
+    //fopen
+    fp = fopen("sample.txt","w");
+    
+    //3번 해야되니까 for문 사용 
+    for (i=0;i<3;i++)
+    {
+    //fprintf
+    printf("input a word:");
+    scanf("%s", input); //%s : 문자 여러개 저장 - 형식 지정자 
+    //&input 할 필요 없음
+    fprintf(fp, "%s\n", input); //다 출력 가능!  fp 넣는거 중요 
+}
+
+    //fcolse
+    fclose(fp);
+
 
   system("PAUSE");	
   return 0;
